b736291bc02d12b0dd452719d866a67bf123ad73
Vulwsztyn <artur.mostowski@student.put.poznan.pl>
2019-03-18 18:46:56 +0100

    Nieposprzatane filtorwanie

--- tomograf.py
+++ tomograf.py
@@ -176,18 +176,24 @@ def dodawanieBresenhama(image, p1, p2, w, normal):
             dodajDoPiksela(image, x, y, w, normal)
 
 
-def normalize(img):
+def normalizeWithOffset(img,offset=(0,0,0,0)):
     min = max = img[0][0]
-
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
+    #offsety są góra,lewo,dół, prawo
+    for i in range(offset[0],img.shape[0]-offset[2]):
+        for j in range(offset[1],img.shape[1]-offset[3]):
             max = max if max > img[i][j] else img[i][j]
             min = min if min < img[i][j] else img[i][j]
     print(min, max)
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
+    for i in range(0, img.shape[0]):
+        for j in range(0, img.shape[1]):
             img[i][j] = (img[i][j] - min) / max
+    img=przytijZera(img)
+    return img
 
+def normalize(img,percent=1.0):
+    hor=int(img.shape[1]*(1-percent))//2
+    vert=int(img.shape[0]*(1-percent))//2
+    return normalizeWithOffset(img,(hor,vert,hor,vert))
 
 def VerticalFiltering(image, mask):
     if len(mask)%2==0:
@@ -206,13 +212,31 @@ def pokaz(img):
     plt.imshow(img, cmap=plt.cm.Greys_r)
     plt.show()
 
+
+def przytijZera(img, offset=(0, 0, 0, 0)):
+
+    for i in range(offset[0], img.shape[0] - offset[2]):
+        for j in range(offset[1], img.shape[1] - offset[3]):
+            if img[i][j]<0:
+                img[i][j]=0
+            elif img[i][j]>1:
+                img[i][j] = 1
+    return img
+
+def obniz(img,n, offset=(0, 0, 0, 0)):
+    for i in range(offset[0], img.shape[0] - offset[2]):
+        for j in range(offset[1], img.shape[1] - offset[3]):
+            img[i][j]-=n
+    przytijZera(img)
+    return img
+
 def tomographing(image, alphaStep, SensorCount, theta):
     r = math.sqrt(image.shape[0] ** 2 + image.shape[1] ** 2) / 2
     imageMiddle = (image.shape[0] // 2, image.shape[1] // 2)
     sinogram = []
     emitters = []
     sensors = []
-    katy = range(0, 360, alphaStep)
+    katy = np.arange(0, 360, alphaStep)
     print('Tworzenie sinogramu')
     for i in katy:
         kolumnaSinogramu = []
@@ -220,14 +244,17 @@ def tomographing(image, alphaStep, SensorCount, theta):
         emitters.append(emitterPos)
         sensorPos = sensorPosition(i, r, SensorCount, theta, imageMiddle)
         sensors.append(sensorPos)
-        print('\r{}/360'.format(i + katy[1]-katy[0]), end='')
+        # print('\r{}/360'.format(i + katy[1]-katy[0]), end='')
         for sensor in sensorPos:
             kolumnaSinogramu.append(sredniaBresenhama(image, emitterPos, sensor))
         sinogram.append(kolumnaSinogramu)
     sinogram = np.array(sinogram).T
+
     pokaz(sinogram)
     # Filtrowanie
-    sinogram = VerticalFiltering(np.array(sinogram), [-1,3,-1])
+    sinogram = normalize(sinogram)
+    sinogram = VerticalFiltering(np.array(sinogram), [-3,7,-3])
+    sinogram=przytijZera(sinogram)
     pokaz(sinogram)
     print('\nTworzenie rekonstrukcji')
     # Init Rekonstrukcji
@@ -236,29 +263,37 @@ def tomographing(image, alphaStep, SensorCount, theta):
 
     # Rekonstrukcja
     for i in range(len(katy)):
-        print('\r{}/360'.format(katy[i] + katy[1]-katy[0]), end='')
+        # print('\r{}/360'.format(katy[i] + katy[1]-katy[0]), end='')
         for j, sensor in enumerate(sensors[i]):
             dodawanieBresenhama(reconstructed, emitters[i], sensors[i][j], sinogram[j][i], normal)
     reconstructed = np.array(reconstructed)
+    pokaz(reconstructed)
     # Normalizacja
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             reconstructed[i][j] /= normal[i][j]
-    normalize(reconstructed)
+    normalize(reconstructed,0.08)
     pokaz(reconstructed)
+    x=0.05
+    for i in range(math.ceil(1/x)-10):
+        obniz(reconstructed,x)
+        pokaz(reconstructed)
     return sinogram, reconstructed, normal
 
 
-def tomograf(filePath):
+def tomograf(filePath=0):
     print(filePath)
     image = imread(data_dir + "/phantom.png", as_gray=True)
-    # image = imread("/obrazy/Kolo.png", as_gray=True)
+    # image = imread("./obrazy/Kolo.jpg", as_gray=True)
     image = rescale(image, scale=0.4, mode='reflect', multichannel=False)
     # plt.imshow(image, cmap=plt.cm.Greys_r)
     # plt.show()
-    sinogram, reconstruction, normal = tomographing(image, alphaStep=5, SensorCount=90, theta=180)
+    sinogram, reconstruction, normal = tomographing(image, alphaStep=0.5, SensorCount=181, theta=270)
     diaplyAll(image, sinogram, reconstruction, normal)
-    theta = np.linspace(0., 180., max(image.shape), endpoint=False)
-    sinogram = sinogramWithSkimage(image, theta)
-    reconstruction = backProjectionWithSkimage(sinogram, theta)
+    # theta = np.linspace(0., 180., max(image.shape), endpoint=False)
+    # sinogram = sinogramWithSkimage(image, theta)
+    # reconstruction = backProjectionWithSkimage(sinogram, theta)
     # diaplyAll(image, sinogram, reconstruction, normal)
+
+if __name__=='__main__':
+    tomograf(0)
